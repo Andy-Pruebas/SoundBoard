a3cb3d8d6b4db7a750095ba3d075d21214743d76
GroverLazaro <grover.lazaro@tecsup.edu.pe>
2020-05-14 17:19:37 -0500

    Commit Gave1

---
+++ SoundBoard/SounViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SounViewController.swift
+//  SoundBoard
+//
+//  Created by mbtec22 on 5/14/20.
+//  Copyright © 2020 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class SounViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
