5bb5a511125e4f3918b707841377563d7fee2091
Diego Anchillo <diego.anchillo@tecsup.edu.pe>
2020-05-14 18:25:26 -0500

    commit Diego 2

--- SoundBoard/SounViewController.swift
+++ SoundBoard/SounViewController.swift
@@ -5,29 +5,29 @@ class SounViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
-    var audioRecorder : AvAudioRecorder?
+    var audioRecorder : AVAudioRecorder?
     	
     override func viewDidLoad() {
         super.viewDidLoad()
         setupRecorder()
     }
     func setupRecorder(){
-        do{
+        do{
             let session = AVAudioSession.sharedInstance()
             try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
             try session.overrideOutputAudioPort(.speaker)
             try session.setActive(true)
 
             let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-            let pathComponents = [basePath,"audio.m4a"]
+            let pathComponents = [basePath,"audio.m4a"]
             let audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
             
-            var settings : [String:AnyObject] = [:]
-            settings[AVFormationIDKey] = In(kAudioFormatMPEG4AAC) as AbyObject?
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
             settings[AVSampleRateKey] = 44100.0 as AnyObject?
-            settings[AVNumerOfChannelsKey] = 2 as AnyObject?
-
-            audioRecorder = try AAudioRecorder(url:audioURL,settings:settings)
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            audioRecorder = try AVAudioRecorder(url:audioURL,settings:settings)
             audioRecorder!.prepareToRecord()
         }catch let error as NSError{
             print(error)
