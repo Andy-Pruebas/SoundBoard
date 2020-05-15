ace3cbe2f2d2433dacbedd11843dce9840bbb76d
Diego Anchillo <diego.anchillo@tecsup.edu.pe>
2020-05-15 14:41:40 -0500

    commit Diego 3

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -33,13 +33,27 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         cell.textLabel?.text = sound.name
         return cell
     }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let sound = sounds[indexPath.row]
+    func tableView(_ tableView:UITableView, didSelectRowAt indexPath: IndexPath){
+        let sound = sounds[indexPath.row]
         do{
             audioPlayer = try AVAudioPlayer(data: sound.audio! as Data)
             audioPlayer?.play()
         }catch{}
-        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath	, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle:UITableViewCellEditingStyle ,forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let sound = sounds[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(sound)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                sounds = try context.fetch(Sound.fetchRequest())
+                tableView.reloadData()
+            }catch{}
+        }
+    }
+    
 }
 
